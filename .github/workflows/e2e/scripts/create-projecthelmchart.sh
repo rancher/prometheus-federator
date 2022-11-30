905f2c4bfd6ab0d2e152c3b2449fe1470b643452
Arvind Iyengar <arvind.iyengar@rancher.com>
2022-11-29 16:00:07 -0800

    Add more error logs for failing to create ProjectHelmChart

--- .github/workflows/e2e/scripts/create-projecthelmchart.sh
+++ .github/workflows/e2e/scripts/create-projecthelmchart.sh
@@ -11,6 +11,20 @@ else
     kubectl apply -f ./examples/example.yaml
 fi
 sleep ${DEFAULT_SLEEP_TIMEOUT_SECONDS};
+
+if ! kubectl get -n cattle-monitoring-system job/helm-install-cattle-project-p-example-monitoring; then
+    echo "ERROR: Helm Install Job for Project Monitoring Stack was never created after ${KUBECTL_WAIT_TIMEOUT} seconds"
+    echo "PROJECT HELM CHARTS:"
+    kubectl get projecthelmchart -n cattle-project-p-example -o yaml
+    echo "HELM CHARTS:"
+    kubectl get helmcharts -n cattle-monitoring-system -o yaml
+    echo "HELM RELEASES:"
+    kubectl get helmreleases -n cattle-monitoring-system -o yaml
+    echo "PROMETHEUS FEDERATOR:"
+    kubectl logs deployment/prometheus-federator -n cattle-monitoring-system
+    exit 1
+fi
+
 if ! kubectl wait --for=condition=complete --timeout="${KUBECTL_WAIT_TIMEOUT}" -n cattle-monitoring-system job/helm-install-cattle-project-p-example-monitoring; then
     echo "ERROR: Helm Install Job for Project Monitoring Stack never completed after ${KUBECTL_WAIT_TIMEOUT} seconds"
     kubectl logs job/helm-install-cattle-project-p-example-monitoring -n cattle-monitoring-system
