11fe1c71226928c8dbad22391770e337368fb71b
Arvind Iyengar <arvind.iyengar@rancher.com>
2022-11-29 16:00:30 -0800

    Fix E2E CI to work on a k3s and rke2 cluster where the Helm Controller is fixed

--- .github/workflows/e2e/scripts/install-federator.sh
+++ .github/workflows/e2e/scripts/install-federator.sh
@@ -9,13 +9,55 @@ latest_chart=$(find ./charts/prometheus-federator -type d -maxdepth 1 -mindepth 
 
 case "${KUBERNETES_DISTRIBUTION_TYPE}" in
 "k3s")
-    cluster_args="--set helmProjectOperator.helmController.enabled=false"
+    cluster_args=""
+    kubernetes_version=$(kubectl version --short | grep "Server Version" | cut -d ' ' -f3)
+    case "${kubernetes_version}" in
+    v1.23.*)
+        embedded_helm_controller_fixed_version="v1.23.14"
+        if [[ $(echo ${kubernetes_version} ${embedded_helm_controller_fixed_version} | tr " " "\n" | sort -rV | head -n 1 ) == "${embedded_helm_controller_fixed_version}" ]]; then
+            cluster_args="--set helmProjectOperator.helmController.enabled=false"
+        fi
+        ;;
+    v1.24.*)
+        embedded_helm_controller_fixed_version="v1.24.8"
+        if [[ $(echo ${kubernetes_version} ${embedded_helm_controller_fixed_version} | tr " " "\n" | sort -rV | head -n 1 ) == "${embedded_helm_controller_fixed_version}" ]]; then
+            cluster_args="--set helmProjectOperator.helmController.enabled=false"
+        fi
+        ;;
+    *)
+        embedded_helm_controller_fixed_version="v1.25.4"
+        if [[ $(echo ${kubernetes_version} ${embedded_helm_controller_fixed_version} | tr " " "\n" | sort -rV | head -n 1 ) == "${embedded_helm_controller_fixed_version}" ]]; then
+            cluster_args="--set helmProjectOperator.helmController.enabled=false"
+        fi
+        ;;
+    esac
     ;;
 "rke")
     cluster_args=""
     ;;
 "rke2")
-    cluster_args="--set helmProjectOperator.helmController.enabled=false"
+    cluster_args=""
+    kubernetes_version=$(kubectl version --short | grep "Server Version" | cut -d ' ' -f3)
+    case "${kubernetes_version}" in
+    v1.23.*)
+        embedded_helm_controller_fixed_version="v1.23.14"
+        if [[ $(echo ${kubernetes_version} ${embedded_helm_controller_fixed_version} | tr " " "\n" | sort -rV | head -n 1 ) == "${embedded_helm_controller_fixed_version}" ]]; then
+            cluster_args="--set helmProjectOperator.helmController.enabled=false"
+        fi
+        ;;
+    v1.24.*)
+        embedded_helm_controller_fixed_version="v1.24.8"
+        if [[ $(echo ${kubernetes_version} ${embedded_helm_controller_fixed_version} | tr " " "\n" | sort -rV | head -n 1 ) == "${embedded_helm_controller_fixed_version}" ]]; then
+            cluster_args="--set helmProjectOperator.helmController.enabled=false"
+        fi
+        ;;
+    *)
+        embedded_helm_controller_fixed_version="v1.25.4"
+        if [[ $(echo ${kubernetes_version} ${embedded_helm_controller_fixed_version} | tr " " "\n" | sort -rV | head -n 1 ) == "${embedded_helm_controller_fixed_version}" ]]; then
+            cluster_args="--set helmProjectOperator.helmController.enabled=false"
+        fi
+        ;;
+    esac
     ;;
 *)
     echo "KUBERNETES_DISTRIBUTION_TYPE=${KUBERNETES_DISTRIBUTION_TYPE} is unknown"
