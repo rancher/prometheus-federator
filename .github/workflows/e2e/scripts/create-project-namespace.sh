c1d47bcdf1607129afa22e2df7533377cbc276c1
Dan Pock <self@danpock.me>
2024-10-04 15:35:55 -0400

    Adjust ci to use new webhook project label method

--- .github/workflows/e2e/scripts/create-project-namespace.sh
+++ .github/workflows/e2e/scripts/create-project-namespace.sh
@@ -8,6 +8,7 @@ cd $(dirname $0)/../../../..
 
 kubectl create namespace e2e-prometheus-federator || true
 kubectl label namespace e2e-prometheus-federator field.cattle.io/projectId=p-example --overwrite
+kubectl annotate namespace e2e-prometheus-federator field.cattle.io/projectId=local:p-example --overwrite
 sleep "${DEFAULT_SLEEP_TIMEOUT_SECONDS}"
 if ! kubectl get namespace cattle-project-p-example; then
     echo "ERROR: Expected cattle-project-p-example namespace to exist after ${DEFAULT_SLEEP_TIMEOUT_SECONDS} seconds, not found"
