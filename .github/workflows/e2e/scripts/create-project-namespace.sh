d80a59c1e32f605a9d664fe78d45d33d878aaf11
Dan Pock <self@danpock.me>
2024-10-04 20:43:49 -0400

    update project-namespace creation

--- .github/workflows/e2e/scripts/create-project-namespace.sh
+++ .github/workflows/e2e/scripts/create-project-namespace.sh
@@ -6,9 +6,13 @@ source $(dirname $0)/entry
 
 cd $(dirname $0)/../../../..
 
-kubectl create namespace e2e-prometheus-federator || true
-kubectl label namespace e2e-prometheus-federator field.cattle.io/projectId=p-example --overwrite
-kubectl annotate namespace e2e-prometheus-federator field.cattle.io/projectId=local:p-example --overwrite
+USE_RANCHER=${USE_RANCHER:-"false"}
+if [ "$USE_RANCHER" = "true" ]; then
+  kubectl apply -f ./examples/ci/project.yaml
+fi
+
+kubectl apply -f ./examples/ci/namespace.yaml
+
 sleep "${DEFAULT_SLEEP_TIMEOUT_SECONDS}"
 if ! kubectl get namespace cattle-project-p-example; then
     echo "ERROR: Expected cattle-project-p-example namespace to exist after ${DEFAULT_SLEEP_TIMEOUT_SECONDS} seconds, not found"
