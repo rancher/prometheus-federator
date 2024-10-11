0f0fbdc532d7d54044f4e1b406b56144ccc84d90
Dan Pock <self@danpock.me>
2024-10-11 14:41:23 -0400

    Enable hpo generator

--- generate.go
+++ generate.go
@@ -2,8 +2,8 @@
 //go:generate go run pkg/helm-locker/codegen/main.go
 //go:generate go run ./pkg/helm-locker/codegen crds ./crds/helm-locker/crds.yaml
 
-// //go:generate go run pkg/helm-project-operator/codegen/cleanup/main.go
-// //go:generate go run pkg/helm-project-operator/codegen/main.go
-// //go:generate go run ./pkg/helm-project-operator/codegen crds ./crds/helm-project-operator ./crds/helm-project-operator
+//go:generate go run pkg/helm-project-operator/codegen/cleanup/main.go
+//go:generate go run pkg/helm-project-operator/codegen/main.go
+//go:generate go run ./pkg/helm-project-operator/codegen crds ./crds/helm-project-operator ./crds/helm-project-operator
 
 package main
