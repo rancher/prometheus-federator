fb85fc94a64138bb83515a64b62f46a1d9f4ab2e
Dan Pock <self@danpock.me>
2024-10-24 16:24:07 -0400

    Update CRD creation method to properly set GVK

--- pkg/helm-project-operator/crd/crds.go
+++ pkg/helm-project-operator/crd/crds.go
@@ -18,7 +18,6 @@ import (
 	"github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/rest"
 )
 
@@ -141,14 +140,18 @@ func objects(v1beta1 bool, crdDefs []crd.CRD) (crds []runtime.Object, err error)
 // List returns the list of CRDs and dependent CRDs for this operator
 func List() ([]crd.CRD, []crd.CRD) {
 	crds := []crd.CRD{
-		newCRD(&v1alpha1.ProjectHelmChart{}, func(c crd.CRD) crd.CRD {
-			return c.
-				WithColumn("Status", ".status.status").
-				WithColumn("System Namespace", ".status.systemNamespace").
-				WithColumn("Release Namespace", ".status.releaseNamespace").
-				WithColumn("Release Name", ".status.releaseName").
-				WithColumn("Target Namespaces", ".status.targetNamespaces")
-		}),
+		newCRD(
+			"ProjectHelmChart.helm.cattle.io/v1alpha1",
+			&v1alpha1.ProjectHelmChart{},
+			func(c crd.CRD) crd.CRD {
+				return c.
+					WithColumn("Status", ".status.status").
+					WithColumn("System Namespace", ".status.systemNamespace").
+					WithColumn("Release Namespace", ".status.releaseNamespace").
+					WithColumn("Release Name", ".status.releaseName").
+					WithColumn("Target Namespaces", ".status.targetNamespaces")
+			},
+		),
 	}
 	crdDeps := append(helmcontrollercrd.List(), helmlockercrd.List()...)
 	return crds, crdDeps
@@ -165,15 +168,11 @@ func Create(ctx context.Context, cfg *rest.Config) error {
 	return factory.BatchCreateCRDs(ctx, append(crds, crdDeps...)...).BatchWait()
 }
 
-func newCRD(obj interface{}, customize func(crd.CRD) crd.CRD) crd.CRD {
-	crd := crd.CRD{
-		GVK: schema.GroupVersionKind{
-			Group:   "helm.cattle.io",
-			Version: "v1alpha1",
-		},
-		Status:       true,
-		SchemaObject: obj,
-	}
+func newCRD(namespacedType string, obj interface{}, customize func(crd.CRD) crd.CRD) crd.CRD {
+	crd := crd.NamespacedType(namespacedType).
+		WithSchemaFromStruct(obj).
+		WithStatus()
+
 	if customize != nil {
 		crd = customize(crd)
 	}
