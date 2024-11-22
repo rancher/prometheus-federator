4bb53bfe3963bf1e54adf58688e7649e56a7d01a
Dan Pock <self@danpock.me>
2024-11-22 14:30:37 -0500

    fix e2e CI registry name

--- .github/workflows/e2e/scripts/hpo-install-helm-project-operator.sh
+++ .github/workflows/e2e/scripts/hpo-install-helm-project-operator.sh
@@ -8,6 +8,6 @@ cd $(dirname $0)/../../../..
 
 latest_chart=./packages/helm-project-operator/charts
 
-helm upgrade --install --create-namespace -n cattle-helm-system helm-project-operator --set image.repository=${REPO:-rancher}/helm-project-operator --set image.tag=${TAG:-dev} ${cluster_args} ${RANCHER_HELM_ARGS} ${latest_chart}
+helm upgrade --install --create-namespace -n cattle-helm-system helm-project-operator --set image.registry='',image.repository=${REPO:-rancher}/helm-project-operator,image.tag=${TAG:-dev} ${cluster_args} ${RANCHER_HELM_ARGS} ${latest_chart}
 
 echo "PASS: Helm Project Operator has been installed"
