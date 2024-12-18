a864192fdace6ae50058d039a158f52ab997e9d1
Dan Pock <self@danpock.me>
2024-12-18 13:00:28 -0500

    Add flag to only dump Chart.yaml from embedded

--- pkg/debug/chart.go
+++ pkg/debug/chart.go
@@ -10,19 +10,23 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/sirupsen/logrus"
+
 	"github.com/spf13/cobra"
 )
 
 func ChartDebugSubCommand(base64ChartTarball string) *cobra.Command {
-	return &cobra.Command{
+	chartDebug := &cobra.Command{
 		Use:   "debug-chart",
 		Short: "This command helps debug the internal chart files",
-		Run: func(_ *cobra.Command, _ []string) {
-			fmt.Println("TODO debug")
-			// TODO: use straing data from base64ChartTarball to:
-			// Un-base64 the data to get a raw tgz file,
-			// Prompt for user input - in the future, for now just use local CWD,
-			// Save the files of the charts tar extracted to the local CWD
+		Run: func(cmd *cobra.Command, _ []string) {
+			chartOnly, _ := cmd.Flags().GetBool("chart-only")
+			if chartOnly {
+				logrus.Info("Only the embedded chart's `Chart.yaml` will be exported.")
+			} else {
+				logrus.Info("The entire embedded chart wil be exported.")
+			}
+
 			chartTarballData, err := base64.StdEncoding.DecodeString(base64ChartTarball)
 			if err != nil {
 				fmt.Println("error:", err)
@@ -67,6 +71,43 @@ func ChartDebugSubCommand(base64ChartTarball string) *cobra.Command {
 				// Determine the path to extract the file to
 				filePath := filepath.Join(debugDir, header.Name)
 
+				if chartOnly {
+					switch header.Typeflag {
+					case tar.TypeReg:
+						if header.Name == "rancher-project-monitoring/Chart.yaml" {
+							logrus.Info("Found a `Chart.yaml` file to export.")
+							// Ensure the parent directory exists
+							parentDir := filepath.Dir(filePath)
+							if err := os.MkdirAll(parentDir, 0755); err != nil {
+								logrus.Error("Error creating parent directory:", err)
+								return
+							}
+
+							// Create regular file
+							outFile, err := os.Create(filePath)
+							if err != nil {
+								logrus.Error("Error creating file:", err)
+								return
+							}
+
+							// Copy file content
+							if _, err := io.Copy(outFile, tarReader); err != nil {
+								logrus.Error("Error writing file content:", err)
+								outFile.Close()
+								return
+							}
+							logrus.Info("The `Chart.yaml` file was exported")
+							outFile.Close()
+						} else {
+							logrus.Debugf("Skipping file: %s\n", header.Name)
+						}
+					default:
+						logrus.Debugf("Skipping file: %s\n", header.Name)
+					}
+
+					return
+				}
+
 				switch header.Typeflag {
 				case tar.TypeDir:
 					// Create directory
@@ -104,4 +145,6 @@ func ChartDebugSubCommand(base64ChartTarball string) *cobra.Command {
 			fmt.Println("Chart files successfully extracted to", debugDir)
 		},
 	}
+	chartDebug.PersistentFlags().BoolP("chart-only", "C", false, "When set, only the `Chart.yaml` will be exported.")
+	return chartDebug
 }
