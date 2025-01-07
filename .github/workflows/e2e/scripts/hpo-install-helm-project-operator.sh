58dc48a619fb6e291b48de885bf2018fd01018c9
Dan Pock <self@danpock.me>
2025-01-06 19:17:08 -0500

    adjust e2e install process

--- .github/workflows/e2e/scripts/hpo-install-helm-project-operator.sh
+++ .github/workflows/e2e/scripts/hpo-install-helm-project-operator.sh
@@ -10,11 +10,8 @@ source "$(pwd)/scripts/util-team-charts"
 NEWEST_CHART_VERSION=$(newest-chart-version "helm-project-operator")
 fetch-team-chart "helm-project-operator" "$NEWEST_CHART_VERSION"
 LATEST_CHART_PATH="./build/charts/helm-project-operator-${NEWEST_CHART_VERSION}.tgz"
+tar -xvzf "$LATEST_CHART_PATH" -C ./build/charts/
 
-helm upgrade --install --create-namespace -n cattle-helm-system helm-project-operator \
-  --set "image.registry='',image.repository=${REPO:-rancher}/helm-project-operator,image.tag=${TAG:-dev}" \
-  "${cluster_args}" \
-  "${RANCHER_HELM_ARGS}" \
-  "${LATEST_CHART_PATH}"
+helm upgrade --install --create-namespace -n cattle-helm-system helm-project-operator --set image.registry='',image.repository=${REPO:-rancher}/helm-project-operator,image.tag=${TAG:-dev} ${cluster_args} ${RANCHER_HELM_ARGS} ./build/charts/helm-project-operator
 
 echo "PASS: Helm Project Operator has been installed"
