5e495a8b30f0a0aed496531231a3f444bb736630
Julia Bier <juliabiersuriano@gmail.com>
2025-01-29 15:49:22 -0400

    Added debug logs for HelmRelease controller

--- pkg/helm-locker/controllers/release/controller.go
+++ pkg/helm-locker/controllers/release/controller.go
@@ -96,9 +96,13 @@ func Register(
 }
 
 func (h *handler) OnObjectSetChange(setID string, obj runtime.Object) (runtime.Object, error) {
+	logrus.Infof("Handling ObjectSet %s", setID)
+
 	helmReleases, err := h.helmReleaseCache.GetByIndex(HelmReleaseByReleaseKey, setID)
 	if err != nil {
-		return nil, fmt.Errorf("unable to find HelmReleases for objectset %s to trigger event", setID)
+		err = fmt.Errorf("unable to find HelmReleases for objectset %s to trigger event", setID)
+		logrus.Errorf("%v", err)
+		return nil, err
 	}
 	for _, helmRelease := range helmReleases {
 		if helmRelease == nil {
@@ -194,9 +198,11 @@ func (h *handler) OnHelmRelease(_ string, helmRelease *v1alpha1.HelmRelease) (*v
 	if shouldManage, err := h.shouldManage(helmRelease); err != nil {
 		return helmRelease, err
 	} else if !shouldManage {
+		logrus.Infof("HelmRelease %s/%s will not be managed by this operator.", helmRelease.Name, helmRelease.Namespace)
 		return helmRelease, nil
 	}
 	if helmRelease.DeletionTimestamp != nil {
+		logrus.Infof("HelmRelease %s/%s has a non-nil deletion timestamp.", helmRelease.Name, helmRelease.Namespace)
 		return helmRelease, nil
 	}
 	releaseKey := releaseKeyFromRelease(helmRelease)
@@ -211,7 +217,9 @@ func (h *handler) OnHelmRelease(_ string, helmRelease *v1alpha1.HelmRelease) (*v
 			helmRelease.Status.Notes = ""
 			return h.helmReleases.UpdateStatus(helmRelease)
 		}
-		return helmRelease, fmt.Errorf("unable to find latest Helm Release Secret tied to Helm Release %s: %s", helmRelease.GetName(), err)
+		err = fmt.Errorf("unable to find latest Helm Release Secret tied to Helm Release %s: %s", helmRelease.GetName(), err)
+		logrus.Infof("%v", err)
+		return helmRelease, err
 	}
 	logrus.Infof("loading latest release version %d of HelmRelease %s", latestRelease.Version, helmRelease.GetName())
 	releaseInfo := newReleaseInfo(latestRelease)
