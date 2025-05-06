679a098a6d101bfee480ec8c37fde35ba76795c2
Alexandre Lamarre <alexandre.lamarre@suse.com>
2025-05-06 11:26:57 -0400

    add more comments
    Signed-off-by: Alexandre Lamarre <alexandre.lamarre@suse.com>

--- internal/integration/prometheus_federator_test.go
+++ internal/integration/prometheus_federator_test.go
@@ -1,3 +1,4 @@
 package integration_test
 
 // TODO : migrate prom-fed e2e test to mirror other implementations
+// might want to split out this test into its own package, it can have synchronization setup rules for setting up upstream rancher-monitoring once
