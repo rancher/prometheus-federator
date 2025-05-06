987265ba70bf2aa071ecd4ba7e38c2c82b13f9b6
Alexandre Lamarre <alexandre.lamarre@suse.com>
2025-05-06 11:21:20 -0400

    test: add better synchronization for test setups
    Signed-off-by: Alexandre Lamarre <alexandre.lamarre@suse.com>

--- internal/integration/integration_suite_test.go
+++ internal/integration/integration_suite_test.go
@@ -7,12 +7,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/rancher/prometheus-federator/internal/test"
-	"github.com/rancher/prometheus-federator/pkg/instrumentation"
 )
-
-func init() {
-	instrumentation.InitTracing("prometheus-federator-integration-tests")
-}
 
 func TestIntegration(t *testing.T) {
 	SetDefaultEventuallyTimeout(60 * time.Second)
@@ -20,15 +15,22 @@ func TestIntegration(t *testing.T) {
 	SetDefaultConsistentlyDuration(5 * time.Second)
 	SetDefaultConsistentlyPollingInterval(50 * time.Millisecond)
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Integration Suite")
+
+	// Dev : to debug specific suites, use the following
+	suiteConfig, reporterConfig := GinkgoConfiguration()
+	// suiteConfig.LabelFilter = "DEBUG"
+	suiteConfig.FocusStrings = []string{"HPO/Namespace/Init"}
+	RunSpecs(t, "Integration Suite", suiteConfig, reporterConfig)
+
+	// comment out the following line to run only the specific suite
+	// RunSpecs(t, "Integration Suite")
 }
 
-// Initialize clients, object trackers and contexts used by the tests
-var _ = BeforeSuite(test.Setup)
-
-// var _ = SynchronizedBeforeSuite(
-// 	test.Setup,
-// 	func(data []byte) {
-// 		// this is invoked after the test suite, this could be things like an audit of which objects
-// 		// are created, etc..
-// 	})
+var _ = SynchronizedBeforeSuite(func() []byte {
+	// setup once across all processes
+	test.SetupOnce()
+	return []byte{}
+}, func(_ []byte) {
+	// setup across all downstream processes
+	test.Setup()
+})
