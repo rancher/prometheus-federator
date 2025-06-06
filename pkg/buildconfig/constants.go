087090f94a3713e7fb6d57f649927f95f821cd79
Julia Bier <juliabiersuriano@gmail.com>
2025-06-06 14:37:30 -0400

    Bump RPM

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -7,5 +7,5 @@ const (
 	K3sTestingMinVersion            = "v1.30.9+k3s1"
 	KuberlrVersion                  = "v4"
 	RancherMonitoringVersion        = "latest"
-	RancherProjectMonitoringVersion = "0.6.0"
+	RancherProjectMonitoringVersion = "0.6.1"
 )
