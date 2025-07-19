542317f86e89dd5ad32e2c642174ca2b43af0b16
Diogo Souza <diogo.souza@suse.com>
2025-07-18 21:03:09 -0300

    bump version of rpm

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -7,5 +7,5 @@ const (
 	K3sTestingMinVersion            = "v1.31.9+k3s1"
 	KuberlrVersion                  = "v5"
 	RancherMonitoringVersion        = "latest"
-	RancherProjectMonitoringVersion = "0.6.2"
+	RancherProjectMonitoringVersion = "0.6.3"
 )
