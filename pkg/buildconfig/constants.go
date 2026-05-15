fa242e14109b7ba44071155460f8752273be2435
Dan Pock <self@danpock.me>
2026-05-15 16:23:33 -0400

    Update build.yaml

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -3,9 +3,9 @@
 package buildconfig
 
 const (
-	K3sTestingMaxVersion            = "v1.35.0+k3s3"
-	K3sTestingMinVersion            = "v1.33.7+k3s3"
-	KuberlrVersion                  = "v7"
-	RancherMonitoringVersion        = "108.0.2+up77.9.1-rancher.11"
+	K3sTestingMaxVersion            = "v1.35.4+k3s1"
+	K3sTestingMinVersion            = "v1.34.7+k3s1"
+	KuberlrVersion                  = "v8"
+	RancherMonitoringVersion        = "109.0.2+up80.9.1-rancher.8"
 	RancherProjectMonitoringVersion = "0.8.0"
 )
