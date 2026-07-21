828e8d59898e4fc804e8ee4acc4dc0544c3da716
Rodolfo Mói de Oliveira <rodolfo.oliveira@suse.com>
2026-07-21 16:23:42 -0300

    fix: Replace yaml library
    Related to https://github.com/rancher/rancher/issues/56157
    
    Signed-off-by: Rodolfo Mói de Oliveira <rodolfo.oliveira@suse.com>

--- internal/helm-project-operator/controllers/common/runtime.go
+++ internal/helm-project-operator/controllers/common/runtime.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/rancher/prometheus-federator/internal/helm-project-operator/apis/helm.cattle.io/v1alpha1"
 	"github.com/sirupsen/logrus"
-	"gopkg.in/yaml.v2"
+	"go.yaml.in/yaml/v2"
 )
 
 type RuntimeOptions struct {
--- pkg/codegen/buildconfig/writer.go
+++ pkg/codegen/buildconfig/writer.go
@@ -10,9 +10,9 @@ import (
 	"strings"
 	"text/template"
 
+	"go.yaml.in/yaml/v3"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
-	"gopkg.in/yaml.v3"
 )
 
 type GoConstantsWriter struct {
